f5a9a6fe77cb88577975c9f6e366b876a0b9d028
Victor <victor.zhou@aftership.com>
2019-03-01 10:33:50 +0800

    use uuid

--- index.js
+++ index.js
@@ -6,35 +6,50 @@ const app = new Koa();
 
 const pubSub = require('./pub_sub');
 
-const {AWS_REGION, AWS_FIFO_URL} = process.env
+const {AWS_REGION, AWS_FIFO_URL: QueueUrl} = process.env
 const AWS = require('aws-sdk');
-AWS.config.update({region: process.env.AWS_REGION});
+AWS.config.update({AWS_REGION});
 const sqs = new AWS.SQS({apiVersion: '2012-11-05'});
 
-const params = {
-    MessageGroupId: 'SAMPLE',
-    MessageBody: Math.random() + 'hello-world-sample',
-    QueueUrl: AWS_FIFO_URL
-  };
-
-//  sqs.sendMessage(params).promise()
 
 const topic = (req_id) => `topic_${req_id}`
+
+setInterval(async () => {
+    const {Messages} = await sqs.receiveMessage({
+        QueueUrl,
+    }).promise()
+
+    if (!Messages) return;
+
+    const tasks = Messages.map(async m => {
+        const {ReceiptHandle, Body} = m
+        const {user_id, req_id} = JSON.parse(Body)
+        await writeToDb(user_id+req_id)
+        await sqs.deleteMessage({QueueUrl, ReceiptHandle}).promise()
+        pubSub.emit(topic(req_id))
+    })
+    await Promise.all(tasks)
+}, 1000)
 
 app
     .use(bodyParser())
     .use(ctx => {
         ctx.type = 'application/json';
         const {user_id, req_id} = ctx.request.body;
+
         return new Promise((resolve) => {
+            
             pubSub.on(topic(req_id), () => {
                 ctx.body = ctx.request.body;
                 resolve()
             })
-            writeToDb(user_id+req_id)
-            .then(() => {
-                pubSub.emit(topic(req_id))
+
+            sqs.sendMessage({
+                QueueUrl,
+                MessageBody: JSON.stringify({user_id, req_id}),
+                MessageGroupId: user_id
             })
+            .promise()
         })
     });
 
