2e1a6a7020aee34bc0e742deda0eeb421b983a2a
Victor <victor.zhou@aftership.com>
2019-06-20 01:07:42 +0800

    :art: simulate cornjob service

--- example-activate-subscription/user.js
+++ example-activate-subscription/user.js
@@ -1,32 +1,52 @@
 "use strict";
 
+const debug = require('debug')('userEventsProcess')
 const canUserChangeStatus = require('./canUserChangeStatus');
 const {delay} = require('bluebird')
 
 const initial_status = "unactivated";
 
+
 // Assumption: append-only stores
 // read-order always the same
 const eventStores = [initial_status]
 
-// Fold it for presentation
-const getEventsUntil = async (event_id) => {
-  const max_id = eventStores.length
-  if (max_id >= event_id) {
-      return eventStores.slice(0, event_id + 1)
-  } else {
-      await delay(500)
-      return getEventsUntil(event_id)
-  }
+const initial_success = true
+const initial_event_id = 0
+const processedEvents = {
+  [initial_event_id]: [initial_success, initial_status]
 }
 
+// Fold it for presentation
+const processEventsCronjob = setInterval(() => {
+  const latest_event_id = eventStores.length - 1
+  debug({
+    eventStores,
+    processedEvents,
+    latest_event_id
+  })
+  if (!processedEvents[latest_event_id]) {
+    const from_event_id = Number(Object.keys(processedEvents).pop())
+    for (let i = from_event_id; i < latest_event_id; i++) {
+      const [_, old_status] = processedEvents[i]
+      const new_status = eventStores[i + 1]
+      const success = canUserChangeStatus(old_status, new_status)
+      processedEvents[i + 1] = [success, new_status]
+      debug({
+        old_status, new_status, success, event_id: i
+      })
+    }
+  }
+}, 3000);
+
 const readRequestResult = async (event_id) => {
-  const events = await getEventsUntil(event_id) // Query
-  const [event_success] = events.reduce(
-      ([success, old_status], new_status) =>
-          canUserChangeStatus(old_status, new_status) ? [true, new_status] : [false, old_status]
-  )
-  return [event_success, events]
+  const result = processedEvents[event_id]
+  if (!result) {
+    await delay(500)
+    return readRequestResult(event_id)
+  }
+  const [success] = result
+  return [success, processedEvents]
 }
 
 const requestToUpdateSubscriptionStatus = async to_status => {
