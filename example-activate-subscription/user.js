bb50da811c49238f7330ebe94fc1ce2ea11a6b96
Victor <victor.zhou@aftership.com>
2019-06-21 14:22:40 +0800

    :fire: Remove useless statement

--- example-activate-subscription/user.js
+++ example-activate-subscription/user.js
@@ -29,7 +29,6 @@ const readRequestResult = async (event_id) => {
 }
 
 const requestToUpdateSubscriptionStatus = async to_status => {
-  await delay(2000);
   // return index of the last element
   eventStores.push(to_status)
   const event_id = eventStores.length - 1
